215489bf3523e9bbf1e398a0f3e6c48becccdeca
Kebre001 <kebre001@gmail.com>
2015-01-14 19:13:08 +0100

    Check every 5th s

--- target/WhereYouAtMaven-1.0-SNAPSHOT/JS/nodeChat.js
+++ target/WhereYouAtMaven-1.0-SNAPSHOT/JS/nodeChat.js
@@ -58,12 +58,58 @@
     }
     
     function oneSecondFunction() {
-    // stuff you want to do every second
-    console.log("1");
+        // stuff you want to do every second
+        console.log("CHECKING DATES FOR NEW MESSAGES");
+            
+            var GameScore = Parse.Object.extend("Messages");
+            var query = new Parse.Query(GameScore);
+            
+            var GameScore2 = Parse.Object.extend("Messages");
+            var query2 = new Parse.Query(GameScore2);
+            
+            query.equalTo("sender", document.getElementById("receiveOld:sender").value);
+            query.equalTo("receiver", document.getElementById("receiveOld:receiver").value);
+            
+            query2.equalTo("receiver", document.getElementById("receiveOld:sender").value);
+            query2.equalTo("sender", document.getElementById("receiveOld:receiver").value);
+            
+            var mainQuery = Parse.Query.or(query, query2);
+            
+            mainQuery.descending("createdAt");
+            mainQuery.limit(1);
+            
+            mainQuery.find({
+              success: function(results) {
+                // Do something with the returned Parse.Object values
+                var latestDate = new Date(latestMessage);
+                for (var i = 0; i < results.length; i++) {
+                    
+                    var newDate = new Date(results[i].createdAt);
+                    
+                    if(newDate > latestDate){
+                        var object = results[i];
+                        console.log(object.get("sender") + ' - ' + object.get('message'));
+                        if(object.get('sender') == document.getElementById("receiveOld:sender").value){
+                            writeToScreen('<span style="color: green;">SENT: ' + object.get('message') + '</span>');
+                        }else{
+                            writeToScreen('<span style="color: blue;"> ' + object.get('message') + '</span>');
+                        }
+                        latestMessage = object.createdAt;
+                        fetched++;
+                    }else{
+                        console.log("NO NEW MESSAGE TO FETCH");
+                    }
+                }
+                console.log("LATEST MESSAGE: " + latestMessage);
+              },
+              error: function(error) {
+                alert("Error: " + error.code + " " + error.message);
+              }
+            });
     }
     
     $(function(){
-    setInterval(oneSecondFunction, 1000);
+    setInterval(oneSecondFunction, 5000);
     });
 
     window.addEventListener("load", init, false);
